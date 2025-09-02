59b83fb0755418919ef969485564366973f87aa4
Skok Oleg <o.skok@yandex.ru>
2025-09-02 15:25:26 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -2,22 +2,32 @@
 #   ZSH ANSIBLE PLUGIN
 # =============================================================================
 
-_zsh_package_pyenv() {
-  git clone "https://github.com/pyenv/pyenv.git"            "${HOME}/.pyenv"
-  git clone "https://github.com/pyenv/pyenv-doctor.git"     "${HOME}/.pyenv/plugins/pyenv-doctor"
-  git clone "https://github.com/pyenv/pyenv-installer.git"  "${HOME}/.pyenv/plugins/pyenv-installer"
-  git clone "https://github.com/pyenv/pyenv-update.git"     "${HOME}/.pyenv/plugins/pyenv-update"
-  git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${HOME}/.pyenv/plugins/pyenv-virtualenv"
-  git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${HOME}/.pyenv/plugins/pyenv-which-ext"
+_zsh_pyenv_package() {
+  export PYENV_ROOT="${HOME}/.pyenv"
+
+  git clone "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}"
+  git clone "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
+  git clone "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
+  git clone "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
+  git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
+  git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
+
+  export PATH="${PATH}:${PYENV_ROOT}/bin"
+  export PATH="${PATH}:${PYENV_ROOT}/shims"
+  rehash
+}
+
+_zsh_pyenv_apply() {
+    eval "$(pyenv init -)"
+    source ${PYENV_ROOT}/completions/pyenv.zsh
+  fi
 }
 
 # Вызов установки в фоне
 if type git >/dev/null; then
-  _zsh_package_pyenv
+  _zsh_pyenv_package
 fi
 
-if [[ -f "${HOME}/.pyenv/bin/pyenv" ]]; then
-  export PYENV_ROOT="${PYENV_HOME}"
-  export PATH="${PATH}:${PYENV_ROOT}/bin"
-  export PATH="${PATH}:${PYENV_ROOT}/shims"
+if type pyenv >/dev/null; then
+  _zsh_pyenv_apply
 fi
