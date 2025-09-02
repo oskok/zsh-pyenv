ed9817085f9343c482a6e4bf3c80d525623029cf
Skok Oleg <o.skok@yandex.ru>
2025-09-02 15:45:35 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -5,14 +5,36 @@
 _zsh_pyenv_package() {
   export PYENV_ROOT="${HOME}/.pyenv"
 
-  if [[ ! -d "${PYENV_ROOT}" ]]; then
-    git clone "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}"
-    git clone "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
-    git clone "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
-    git clone "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
-    git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
-    git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
-  fi
+  typeset -A ZSH_PYENV_SOURCES
+  ZSH_PYENV_SOURCES=(
+    "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}/"
+    "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
+    "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
+    "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
+    "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
+    "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
+  )
+
+for k in "${(@k)ZSH_PYENV_SOURCES}"; do
+  local ZSH_PYENV_URL=$k
+  local ZSH_PYENV_NAME=$(echo ${ZSH_PYENV_URL##*/} | cut -d. -f1)
+  local ZSH_PYENV_PATH=$ZSH_PYENV_SOURCES[$k]
+
+  git clone ${ZSH_PYENV_URL} ${PYENV_ROOT}/${ZSH_PYENV_NAME}/
+done
+
+
+
+
+  # setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
+  # if [[ ! -d "${PYENV_ROOT}" ]]; then
+  #   git clone "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}" 2>&1
+  #   git clone "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"  2>&1
+  #   git clone "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer" 2>&1
+  #   git clone "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"  2>&1
+  #   git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"  2>&1
+  #   git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext" 2>&1
+  # fi
 
   export PATH="${PATH}:${PYENV_ROOT}/bin"
   export PATH="${PATH}:${PYENV_ROOT}/shims"
@@ -20,9 +42,7 @@ _zsh_pyenv_package() {
 }
 
 _zsh_pyenv_apply() {
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init - --no-rehash zsh)"
-  eval "$(pyenv virtualenv-init - zsh)"
+  eval "$(pyenv init)"
 
   source ${PYENV_ROOT}/completions/pyenv.zsh
 }
