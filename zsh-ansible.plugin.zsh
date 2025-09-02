74ee571d52f18b67528b0260d9d6b63e6e680996
Skok Oleg <o.skok@yandex.ru>
2025-09-02 15:56:51 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -5,14 +5,13 @@
 _zsh_pyenv_package() {
   export PYENV_ROOT="${HOME}/.pyenv"
 
-  typeset -A ZSH_PYENV_BIN
-  ZSH_PYENV_BIN=(
+  typeset -A ZSH_PYENV_DEFAULT
+  ZSH_PYENV_DEFAULT=(
     "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}"
   )
 
   typeset -A ZSH_PYENV_PLUGINS
   ZSH_PYENV_PLUGINS=(
-    "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}"
     "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
     "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
     "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
@@ -20,26 +19,13 @@ _zsh_pyenv_package() {
     "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
   )
 
-  for k in "${(@k)ZSH_PYENV_BIN}"; do
-    git clone $k $ZSH_PYENV_BIN[$k]
+  for k in "${(@k)ZSH_PYENV_DEFAULT}"; do
+    git clone $k $ZSH_PYENV_DEFAULT[$k]
   done
 
   for k in "${(@k)ZSH_PYENV_PLUGINS}"; do
     git clone $k $ZSH_PYENV_PLUGINS[$k]
   done
-
-
-
-
-  # setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
-  # if [[ ! -d "${PYENV_ROOT}" ]]; then
-  #   git clone "https://github.com/pyenv/pyenv.git"            "${PYENV_ROOT}" 2>&1
-  #   git clone "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"  2>&1
-  #   git clone "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer" 2>&1
-  #   git clone "https://github.com/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"  2>&1
-  #   git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"  2>&1
-  #   git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext" 2>&1
-  # fi
 
   export PATH="${PATH}:${PYENV_ROOT}/bin"
   export PATH="${PATH}:${PYENV_ROOT}/shims"
@@ -47,7 +33,7 @@ _zsh_pyenv_package() {
 }
 
 _zsh_pyenv_apply() {
-  eval "$(pyenv init)"
+  eval "$(pyenv init - zsh)"
 
   source ${PYENV_ROOT}/completions/pyenv.zsh
 }
