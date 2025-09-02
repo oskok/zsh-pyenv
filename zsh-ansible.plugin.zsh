80a5c198e4b705e7ed28bfeb74824855400e6934
Skok Oleg <o.skok@yandex.ru>
2025-09-02 15:08:16 +0500

    Initial commit

--- zsh-ansible.plugin.zsh
+++ zsh-ansible.plugin.zsh
@@ -5,12 +5,12 @@
 export PYENV_HOME="$HOME/.pyenv"
 
 _zsh_package_pyenv() {
-  git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_HOME}"
-  git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_HOME}/plugins/pyenv-doctor"
-  git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_HOME}/plugins/pyenv-installer"
-  git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_HOME}/plugins/pyenv-update"
-  git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_HOME}/plugins/pyenv-virtualenv"
-  git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_HOME}/plugins/pyenv-which-ext"
+  git clone "https://github.com/pyenv/pyenv.git"            "${PYENV_HOME}"
+  git clone "https://github.com/pyenv/pyenv-doctor.git"     "${PYENV_HOME}/plugins/pyenv-doctor"
+  git clone "https://github.com/pyenv/pyenv-installer.git"  "${PYENV_HOME}/plugins/pyenv-installer"
+  git clone "https://github.com/pyenv/pyenv-update.git"     "${PYENV_HOME}/plugins/pyenv-update"
+  git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${PYENV_HOME}/plugins/pyenv-virtualenv"
+  git clone "https://github.com/pyenv/pyenv-which-ext.git"  "${PYENV_HOME}/plugins/pyenv-which-ext"
 }
 
 # Вызов установки в фоне
